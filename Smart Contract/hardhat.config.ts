00dcedb1f60ce99ed6154672ea199c35def491ca
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-23 17:30:57 +0700

    modify network

--- Smart Contract/hardhat.config.ts
+++ Smart Contract/hardhat.config.ts
@@ -6,21 +6,12 @@ const { PRIVATE_KEY } = process.env;
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    xinfin: {
-      url: "https://erpc.xinfin.network",
-      accounts: [PRIVATE_KEY],
-      allowUnlimitedContractSize: true
-    },
-
-    apothem: {
-      url: "https://erpc.apothem.network",
+    theta_testnet: {
+      url: "https://eth-rpc-api-testnet.thetatoken.org/rpc",
       accounts: [PRIVATE_KEY],
       allowUnlimitedContractSize: true
     }
   },
-  // etherscan: {
-  //   apiKey: process.env.API_KEY
-  // }
 };
 
 export default config;
